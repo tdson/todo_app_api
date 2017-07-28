ed692130104a6a908768a423abeefd83ac8d0aeb
Son Tran <sontd.it@gmail.com>
2017-07-28 09:36:27 +0700

    Add model todo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :doorkeeper_authorize!, :get_current_user
+  before_action :doorkeeper_authorize!, :current_user
 
   protected
-  def get_current_user
-    @current_user = User.find_by doorkeeper_token.resource_owner_id if doorkeeper_token
+  def current_user
+    @current_user = User.find_by id: doorkeeper_token.resource_owner_id if doorkeeper_token
   end
 
   def doorkeeper_unauthorized_render_options error: nil
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728055716) do
+ActiveRecord::Schema.define(version: 20170728055717) do
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20170728055716) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
